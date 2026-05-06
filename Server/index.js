6efca9b776968356938bbb4fbc9a2c93e0547ced
Sohail Shaikh <shaikhsohail1822@gmail.com>
2026-05-06 23:49:22 +0530

    integrate Razorpay payment system with order creation and verification endpoints

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,7 @@ import cors from "cors";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import interviewRouter from "./routes/interview.route.js";
+import paymentRouter from "./routes/payment.route.js";
 dotenv.config();
 
 const app = express();
@@ -22,7 +23,8 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
-app.use("/api/interview" , interviewRouter)
+app.use("/api/interview", interviewRouter)
+app.use("/api/payment", paymentRouter)
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
