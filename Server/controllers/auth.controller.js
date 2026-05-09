0af02f6dddbeb6a6708271a10b49982bb5a94d37
Sohail Shaikh <shaikhsohail1822@gmail.com>
2026-05-09 11:46:38 +0530

    fix: update cookie settings in googleAuth for improved security

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -14,12 +14,12 @@ export const googleAuth = async (req, res) => {
         }
 
         let token = await genToken(user._id);
-        res.cookie('token', token, {
-            http: true,
-            secure: false,
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
             sameSite: "none",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        })
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
 
         return res.status(200).json(user);
 
